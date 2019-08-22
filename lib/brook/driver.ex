9c8417f1d28be388d4b6c590067044b43ccd434c
Brian Balser <bbalser@pillartechnology.com>
2019-08-22 10:28:28 -0400

    Implement driver that json encode/decodes data

--- lib/brook/driver.ex
+++ lib/brook/driver.ex
@@ -42,3 +42,35 @@ defmodule Brook.Driver.Default do
     :ok
   end
 end
+
+defmodule Brook.Driver.Json do
+  use GenServer
+  @behaviour Brook.Driver
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def init([]) do
+    {:ok, []}
+  end
+
+  def ack(_ack_ref, _ack_data) do
+    :ok
+  end
+
+  def send_event(type, author, data) do
+    event = %Brook.Event{
+      type: type,
+      author: author,
+      create_ts: DateTime.utc_now() |> DateTime.to_unix(),
+      data: data |> Jason.encode!() |> Jason.decode!(),
+      ack_ref: :ack_ref,
+      ack_data: :ack_data
+    }
+
+    GenServer.cast({:via, Registry, {Brook.Registry, Brook.Server}}, {:process, event})
+
+    :ok
+  end
+end
